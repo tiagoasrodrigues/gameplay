bbe7c75d79adfef054ee8b38e7aebe27de9906ec
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-06-25 11:05:29 -0300

    :wrench: chore: add icon in AC and component Load

--- src/screens/AppointmentCreate/index.tsx
+++ src/screens/AppointmentCreate/index.tsx
@@ -76,7 +76,7 @@ export function AppointmentCreate() {
               <View style={styles.select}>
                 {
                   guild.icon
-                    ? <GuildIcon />
+                    ? <GuildIcon guildId={guild.id} iconId={guild.icon} />
                     : <View style={styles.image}
                     />
                 }
