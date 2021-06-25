7471c3367be0e1e45b0280a00fca821fdb5e76a9
Tiago Augusto <tiagoasrodrigues@protonmail.com>
2021-06-25 15:19:50 -0300

    :wrench: chore: ending screen logout

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Text, View } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
 
 import { useAuth } from '../../hooks/auth';
 
@@ -8,12 +9,29 @@ import { Avatar } from '../Avatar';
 import { styles } from './styles';
 
 export function Profile() {
-  const { user } = useAuth();
+  const { user, signOut } = useAuth();
+
+  function handleSignOut() {
+    Alert.alert('Logout', 'Deseja sair do GamePlay?',
+      [
+        {
+          text: 'Não',
+          style: 'cancel'
+        },
+        {
+          text: 'Sim',
+          onPress: () => signOut()
+        }
+      ]
+    )
+  }
 
   return (
     <View style={styles.container}>
 
-      <Avatar urlImage={user.avatar} />
+      <RectButton onPress={handleSignOut}>
+        <Avatar urlImage={user.avatar} />
+      </RectButton>
 
       <View>
         <View style={styles.user}>
@@ -30,6 +48,7 @@ export function Profile() {
           Hoje é dia de vitória
         </Text>
       </View>
+
     </View>
   );
 }
